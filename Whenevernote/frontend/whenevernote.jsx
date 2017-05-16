4930bae33d24bb1d95acc3654d189ea0c7a0f882
Tyler <tyler.reichle@gmail.com>
2017-05-16 14:02:51 -0700

    Create app and root components, add to entry

--- Whenevernote/frontend/whenevernote.jsx
+++ Whenevernote/frontend/whenevernote.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import configureStore from './store/store';
 
+import Root from './components/root';
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const store = configureStore();
   window.store = store;
 
-  ReactDom.render(<h1>Whenevernote</h1>, root);
+  ReactDom.render(<Root store={store}/>, root);
 });
