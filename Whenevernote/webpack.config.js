3464a1ae2e5d5a25b8f28e9bf38e19265a321ce5
Tyler <tyler.reichle@gmail.com>
2017-05-16 09:16:27 -0700

    Add React and frontend dependencies

---
+++ Whenevernote/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: "frontend/whenevernote.jsx",
+  output: {
+    path: "app/assets/javascripts",
+    filename: "bundle.js",
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};
