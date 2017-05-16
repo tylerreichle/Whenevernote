b60b01144dcec40e777f59c4c66a1570bf1f9035
Tyler <tyler.reichle@gmail.com>
2017-05-16 10:19:15 -0700

    Create session api utils and test on window

---
+++ Whenevernote/frontend/util/session_api_util.js
@@ -0,0 +1,21 @@
+export const signup = user => (
+  $.ajax({
+    method: 'POST',
+    url: '/api/users/',
+    data: { user }
+  })
+);
+
+export const signin = user => (
+  $.ajax({
+    method: 'POST',
+    url: '/api/session/',
+    data: { user }
+  })
+);
+export const signout = () => (
+  $.ajax({
+    method: 'DELETE',
+    url: '/api/session/'
+  })
+);
