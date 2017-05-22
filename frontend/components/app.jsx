a0ddc4d30b507684a2cc77a0dd5c4d3f2ae28c61
Tyler <tyler.reichle@gmail.com>
2017-05-22 11:23:02 -0700

    Refactor new-components out of home. Add switches to load components on correct routes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import {
+  AuthRoute,
+  ProtectedRoute,
+  ProtectedExactRoute
+} from '../util/route_util';
+
 import Home from './home/home';
+import NewNote from './notes/new_note_container';
+import NewNotebook from './notebooks/new_notebook_container';
 
 const App = () => (
-  <Home />
+  <Switch>
+    <Route path="/note/new" component={NewNote} />
+    <Route path="/notebook/new" component={NewNotebook} />
+    <Route path="/" component={Home} />
+  </Switch>
 );
 
 export default App;
