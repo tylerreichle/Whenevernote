77b3b1428643f1d9ca51ef2f8c8278348ea07396
Tyler <tyler.reichle@gmail.com>
2017-05-25 15:13:47 -0700

    Fix alignment and overflow of new note

--- frontend/components/notes/new_note.jsx
+++ frontend/components/notes/new_note.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
+import {
+  Editor,
+  EditorState,
+  RichUtils,
+  convertFromRaw,
+  convertToRaw
+} from 'draft-js';
+
 class NewNote extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { title: '', body: '', notebook_id: 1};
+    this.state = {
+      title: '',
+       body: '',
+       notebook_id: 1,
+       editorState: EditorState.createEmpty()
+    };
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.update = this.update.bind(this);
+    this.onChange = (editorState) => this.setState({ editorState });
+    this.handleKeyCommand = (command) => this._handleKeyCommand.bind(command);
+    this.onTab = (e) => this._onTab(e);
+    this.focus = () => this.refs.editor.focus();
   }
 
   handleSubmit(e) {
@@ -18,11 +35,14 @@ class NewNote extends React.Component {
       this.setState({ title: 'Untitled' });
     }
 
+    const noteBody = convertToRaw(this.state.editorState.getCurrentContent());
+    const body = JSON.stringify(noteBody);
+
     const note = {
+      id: this.state.id,
       title: this.state.title,
-      body: this.state.body,
-      author_id: this.props.currentUser.id,
-      notebook_id: this.state.notebook_id
+      body: body,
+      notebook_id: this.state.notebook_id,
     };
     this.props.createNote(note);
     this.props.history.push("/notes");
@@ -32,7 +52,24 @@ class NewNote extends React.Component {
     return e => this.setState({ [property]: e.target.value });
   }
 
+  _handleKeyCommand(command) {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    } else {
+      return 'not-handled';
+    }
+  }
+
+  _onTab(e) {
+    const maxDepth = 4;
+    this.onChange(RichUtils.onTab(e, this.state.editorState, maxDepth));
+  }
+
   render() {
+    const { editorState } = this.state;
+
     return (
       <section className="new-note">
 
@@ -57,13 +94,17 @@ class NewNote extends React.Component {
             placeholder="Title your note"
             onChange={this.update('title')}/>
 
-          <textarea
-            id="new-body"
-            rows="10"
-            cols="50"
-            value={this.state.body}
-            placeholder="Just start typing..."
-            onChange={this.update('body')}/>
+          <div className="RichEditor-editor" onClick={this.focus}>
+              <Editor
+                editorState={editorState}
+                handleKeyCommand={this.handleKeyCommand}
+                onChange={this.onChange}
+                onTab={this.onTab}
+                placeholder="Just start typing..."
+                spellCheck={true}
+                ref="editor"
+              />
+            </div>
 
         </form>
       </section>
@@ -72,11 +113,3 @@ class NewNote extends React.Component {
 }
 
 export default withRouter(NewNote);
-
-// <div className="detail-notebook">
-//   <img src="https://res.cloudinary.com/dkuqs8yz1/image/upload/v1495234906/notebook.png"/>
-// </div>
-//
-// <div className="detail-tag">
-//   <img src="https://res.cloudinary.com/dkuqs8yz1/image/upload/v1495234906/tag.png"/>
-// </div>
