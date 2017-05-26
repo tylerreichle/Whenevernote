95bfa605b5399a2fc3aa3817b11ecad9c6425dff
Tyler <tyler.reichle@gmail.com>
2017-05-26 11:12:47 -0700

    Convert notebook and tags headers to modals

--- frontend/components/notebooks/notebook_header.jsx
+++ frontend/components/notebooks/notebook_header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ReactModal from 'react-modal';
 
 import NotebooksIndex from './notebooks_index_container';
 
@@ -7,8 +8,13 @@ class NotebookHeader extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { dropdownOpen: false };
-    this.toggleMenu = this.toggleMenu.bind(this);
+  this.state = { modalIsOpen: false };
+  this.openModal = this.openModal.bind(this);
+  this.closeModal = this.closeModal.bind(this);
+  }
+
+  componentWillMount() {
+    ReactModal.setAppElement('body');
   }
 
   componentWillReceiveProps(newProps) {
@@ -17,55 +23,56 @@ class NotebookHeader extends React.Component {
     }
   }
 
-  notebookMenu() {
-    if (this.state.dropdownOpen) {
-      return (
-        <div className="nb-header-dropdown">
-
-          <Link to="/notebook/new">
-            <div className="new-nb-item">
-
-                <div id="new-nb-img" className="new-nb-child"></div>
-                <button className="new-nb-child">Create new notebook</button>
-            </div>
-          </Link>
-
-          <NotebooksIndex
-            note={this.props.note}
-            updateNote={this.props.updateNote}
-            iiCallback={'assign'}
-            notesCount={false}/>
-        </div>
-      );
-    }
+  openModal() {
+    this.setState({ modalIsOpen: true });
   }
 
-  toggleMenu(e) {
-    e.preventDefault();
-    if (this.state.dropdownOpen) {
-      this.setState({ dropdownOpen: false });
-    } else {
-      this.setState({ dropdownOpen: true });
-    }
+  closeModal() {
+    this.setState({ modalIsOpen: false });
   }
 
   render() {
     const title = this.props.notebook.title || '';
 
     return (
-      <div className="nb-header" onClick={this.toggleMenu}>
-        <img
-          id="tiny-notebook"
-          src="https://res.cloudinary.com/dkuqs8yz1/image/upload/v1495234906/notebook.png"/>
+      <div>
+        <div className="nb-header" onClick={this.openModal}>
+          <img
+            id="tiny-notebook"
+            src="https://res.cloudinary.com/dkuqs8yz1/image/upload/v1495234906/notebook.png"/>
 
-        <h3>{title}</h3>
+          <h3>{title}</h3>
 
-        <img
-          id="dropdown-arrow"
-          src="https://res.cloudinary.com/dkuqs8yz1/image/upload/v1495502846/dropdown.png"/>
+          <img
+            id="dropdown-arrow"
+            src="https://res.cloudinary.com/dkuqs8yz1/image/upload/v1495502846/dropdown.png"/>
+        </div>
 
-        {this.notebookMenu()}
+        <ReactModal
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={this.closeModal}
+          contentLabel="Notebooks Header"
+          className="nb-header-dropdown"
+          overlayClassName="clear-overlay">
+
+            <Link to="/notebook/new">
+              <div className="new-nb-item">
+
+                  <div id="new-nb-img" className="new-nb-child"></div>
+                  <button className="new-nb-child">Create new notebook</button>
+              </div>
+            </Link>
+
+            <NotebooksIndex
+              note={this.props.note}
+              updateNote={this.props.updateNote}
+              iiCallback={'assign'}
+              notesCount={false}/>
+
+        </ReactModal>
       </div>
+
+
     );
   }
 }
--- frontend/components/tags/tags_header.jsx
+++ frontend/components/tags/tags_header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ReactModal from 'react-modal';
 
 import TagsIndex from './tags_index_container';
 import { selectTagIds } from '../../reducers/selectors';
@@ -8,8 +9,21 @@ class TagsHeader extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { dropdownOpen: false };
-    this.toggleMenu = this.toggleMenu.bind(this);
+    this.state = { modalIsOpen: false };
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  componentWillMount() {
+    ReactModal.setAppElement('body');
+  }
+
+  openModal() {
+    this.setState({ modalIsOpen: true });
+  }
+
+  closeModal() {
+    this.setState({ modalIsOpen: false });
   }
 
   tagsMenu() {
@@ -35,29 +49,44 @@ class TagsHeader extends React.Component {
     }
   }
 
-  toggleMenu(e) {
-    e.preventDefault();
-    if (this.state.dropdownOpen) {
-      this.setState({ dropdownOpen: false });
-    } else {
-      this.setState({ dropdownOpen: true });
-    }
-  }
-
   render() {
     return (
-      <div className="tags-header" onClick={this.toggleMenu}>
-        <img
-          id="tiny-tag"
-          src="https://res.cloudinary.com/dkuqs8yz1/image/upload/v1495234906/tag.png"/>
+      <div>
+        <div className="tags-header" onClick={this.openModal}>
+          <img
+            id="tiny-tag"
+            src="https://res.cloudinary.com/dkuqs8yz1/image/upload/v1495234906/tag.png"/>
 
-        <h3>Tags</h3>
+          <h3>Tags</h3>
 
-        <img
-          id="dropdown-arrow"
-          src="https://res.cloudinary.com/dkuqs8yz1/image/upload/v1495502846/dropdown.png"/>
+          <img
+            id="dropdown-arrow"
+            src="https://res.cloudinary.com/dkuqs8yz1/image/upload/v1495502846/dropdown.png"/>
 
-        {this.tagsMenu()}
+        </div>
+
+        <ReactModal
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={this.closeModal}
+          contentLabel="Notebooks Header"
+          className="tags-header-dropdown"
+          overlayClassName="clear-overlay">
+
+          <Link to="/tag/new">
+            <div className="new-tag-item">
+
+                <div id="new-tag-img" className="new-tag-child"></div>
+                <button className="new-tag-child">Create new tag</button>
+            </div>
+          </Link>
+
+          <TagsIndex
+            noteTags={this.props.tags}
+            noteId={this.props.noteId}
+            iiCallback={'assign'}
+          />
+
+        </ReactModal>
       </div>
     );
   }
