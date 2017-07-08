cc9ab3b7a5d8c3e67743a172b20d5dd85db2fb2d
Tyler <tyler.reichle@gmail.com>
2017-07-08 12:42:40 -0700

    Update home to pure function

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -6,17 +6,13 @@ import Notes from '../notes/notes';
 import Notebooks from '../notebooks/notebooks';
 import Tags from '../tags/tags';
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div className="home-container">
-        <AuthRoute path="/" component={SplashPage} />
-        <ProtectedRoute path="/notes" component={Notes} />
-        <ProtectedRoute path="/notebook" component={Notebooks} />
-        <ProtectedRoute path="/tag" component={Tags} />
-      </div>
-    );
-  }
-}
+const Home = () => (
+  <div className="home-container">
+    <AuthRoute path="/" component={SplashPage} />
+    <ProtectedRoute path="/notes" component={Notes} />
+    <ProtectedRoute path="/notebook" component={Notebooks} />
+    <ProtectedRoute path="/tag" component={Tags} />
+  </div>
+);
 
 export default Home;
