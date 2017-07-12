e23b25e89c5518242c0c330be287918fa5749842
Tyler <tyler.reichle@gmail.com>
2017-07-12 14:00:40 -0700

    Update time since update to display Yesterday when over 24hr old

--- frontend/util/app_util.js
+++ frontend/util/app_util.js
@@ -1,17 +1,17 @@
 const timeSince = (date) => {
   const seconds = Math.floor((new Date() - date) / 1000);
-  let interval = Math.floor(seconds / 31536000);
 
-  if (interval > 1) {
-    return `${interval} YEARS AGO`;
-  }
-  interval = Math.floor(seconds / 2592000);
+  let interval = Math.floor(seconds / 2592000);
   if (interval > 1) {
     return `${interval} MONTHS AGO`;
   }
-  interval = Math.floor(seconds / 86400);
+  interval = Math.floor(seconds / 172800);
   if (interval > 1) {
     return `${interval} DAYS AGO`;
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval >= 1) {
+    return 'YESTERDAY';
   }
   interval = Math.floor(seconds / 3600);
   if (interval > 1) {
