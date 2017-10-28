cf6683cffc2e83b3b2a03cc5c2fa5ec44bfbf019
Tyler <tyler@slice.com>
2017-10-28 15:54:02 -0700

    update Root test imports

--- test/Root.test.js
+++ test/Root.test.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 
-import Root from '../frontend/components/root'
-import configureStore from '../frontend/store/store'
+import Root from '../src/components/root'
+import configureStore from '../src/store/store'
 
 describe('<Root />', () => {
-  it('should render without crashing', () => {
+  it('renders without crashing', () => {
     const store = configureStore()
     shallow(<Root store={store} />)
   })
