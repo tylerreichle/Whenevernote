2cb7491f0f6f8f182ddf94dacf765c124e07746b
Tyler <tyler@slice.com>
2017-10-29 10:32:21 -0700

    combine Sidebar container and display. update imports

--- src/components/notebooks/notebooks.jsx
+++ src/components/notebooks/notebooks.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Route } from 'react-router-dom';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Route } from 'react-router-dom'
 
-import Sidebar from '../sidebar/sidebar_container';
-import NotebookShow from '../notebooks/notebook_show_container';
-import NoteDetail from '../notes/note_detail_container';
-import NewNotebook from '../notebooks/new_notebook_container';
+import Sidebar from '../sidebar/sidebar'
+import NotebookShow from '../notebooks/notebook_show_container'
+import NoteDetail from '../notes/note_detail_container'
+import NewNotebook from '../notebooks/new_notebook_container'
 
 const Notebooks = (props) => {
   if (props.location.pathname === '/notebook/new') {
-    return <NewNotebook />;
+    return <NewNotebook />
   }
   return (
     <section className="notebooks">
@@ -17,13 +17,11 @@ const Notebooks = (props) => {
       <Route path="/notebook/:notebookId/notes" component={NotebookShow} />
       <Route path="/notebook/:notebookId/notes/:noteId" component={NoteDetail} />
     </section>
-  );
-};
+  )
+}
 
 Notebooks.propTypes = {
-  location: PropTypes.objectOf(
-    PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  ).isRequired,
-};
+  location: PropTypes.object.isRequired
+}
 
-export default Notebooks;
+export default Notebooks
--- src/components/tags/tags.jsx
+++ src/components/tags/tags.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Route } from 'react-router-dom'
 
-import Sidebar from '../sidebar/sidebar_container';
-import TagShow from '../tags/tag_show_container';
-import NoteDetail from '../notes/note_detail_container';
-import NewTag from '../tags/new_tag_container';
+import Sidebar from '../sidebar/sidebar'
+import TagShow from '../tags/tag_show_container'
+import NoteDetail from '../notes/note_detail_container'
+import NewTag from '../tags/new_tag_container'
 
-export default class Tags extends React.Component {
-  render() {
-    if (this.props.location.pathname === '/tag/new') {
-      return <NewTag />;
-    } else {
-      return (
-        <section className="tags">
-          <Route path="/tag" component={Sidebar} />
-          <Route path="/tag/:tagId/notes" component={TagShow} />
-          <Route path="/tag/:tagId/notes/:noteId" component={NoteDetail} />
-        </section>
-      );
-    }
+const Tags = (props) => {
+  if (props.location.pathname === '/tag/new') {
+    return <NewTag />
   }
+  return (
+    <section className="tags">
+      <Route path="/tag" component={Sidebar} />
+      <Route path="/tag/:tagId/notes" component={TagShow} />
+      <Route path="/tag/:tagId/notes/:noteId" component={NoteDetail} />
+    </section>
+  )
 }
+
+Tags.propTypes = {
+  location: PropTypes.object.isRequired
+}
+
+export default Tags
