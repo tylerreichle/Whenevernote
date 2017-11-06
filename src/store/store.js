2f8b95ae0c7ae1e0ac52a255772094a017eb92a0
Tyler <tyler@slice.com>
2017-11-05 18:23:00 -0800

    add redux dev tools

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,22 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import RootReducer from '../reducers/root_reducer'
 
-const configureStore = (preloadedState = {}) => (
-  createStore(
+const configureStore = (preloadedState = {}) => {
+  let middleware = applyMiddleware(thunk)
+
+  if (process.env.NODE_ENV !== 'production') {
+    const { devToolsExtension } = window
+    if (typeof devToolsExtension === 'function') {
+      middleware = compose(middleware, devToolsExtension())
+    }
+  }
+
+  return createStore(
     RootReducer,
     preloadedState,
-    applyMiddleware(thunk)
+    middleware
   )
-)
+}
 
 export default configureStore
